db47d94335be3282ad223718ab23204ce9749efd
napman <kotaro.nakayama@gmail.com>
2015-08-14 17:05:44 +0900

    updated

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # settings
-packages="emacs24-nox htop byobu gcc make python lm-sensors nkf ssh ntp git curl ipython python-pip ntfs-3g"
+packages="emacs24-nox htop byobu gcc make python lm-sensors nkf ssh ntp git curl ipython python-pip ntfs-3g whois"
 
 # install packages
 sudo apt-get update
