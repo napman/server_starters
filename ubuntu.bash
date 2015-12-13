bdb6e737b1c96fe99918bd0358fcfce709138ab4
napman <kotaro.nakayama@gmail.com>
2015-12-13 15:06:28 +0900

    excluded ipython

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # settings
-packages="emacs24-nox htop byobu gcc make python lm-sensors nkf ssh ntp git curl ipython python-pip ntfs-3g whois"
+packages="emacs24-nox htop byobu gcc make python lm-sensors nkf ssh ntp git curl python-pip ntfs-3g whois"
 
 # install packages
 sudo apt-get update
 sudo apt-get install -y --force-yes $packages
 
 echo everything is installed well. next -> napmanlib installers
-
